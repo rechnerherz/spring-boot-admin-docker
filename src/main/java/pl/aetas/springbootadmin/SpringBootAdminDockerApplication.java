bffe5bfafdafa1825ab6bc17d7a7270450f389b4
Dario Seidl <darioseidl@fastmail.fm>
2018-07-20 23:57:29 +0200

    Upgrade to Spring Boot Admin 2.0.1

--- src/main/java/pl/aetas/springbootadmin/SpringBootAdminDockerApplication.java
+++ src/main/java/pl/aetas/springbootadmin/SpringBootAdminDockerApplication.java
@@ -1,14 +1,15 @@
 package pl.aetas.springbootadmin;
 
-import de.codecentric.boot.admin.config.EnableAdminServer;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
 @EnableAdminServer
 public class SpringBootAdminDockerApplication {
-
-  public static void main(String[] args) {
-    SpringApplication.run(SpringBootAdminDockerApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminDockerApplication.class, args);
+    }
 }

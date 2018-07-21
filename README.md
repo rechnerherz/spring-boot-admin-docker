8abaad8fc0e606c4310810d09b290b8151de19af
Dario Seidl <darioseidl@fastmail.fm>
2018-07-21 02:02:11 +0200

    Add Spring security HTTP basic auth and login page

--- README.md
+++ README.md
@@ -8,17 +8,19 @@ It was forked from <https://github.com/maniekq/spring-boot-admin-docker> to supp
 
 The Docker image is available as [darioseidl/spring-boot-admin-docker](https://hub.docker.com/r/darioseidl/spring-boot-admin-docker/).
 
-You can run it with:
+Run it with (replace `YOUR_PASSWORD`):
 
 `
-docker run -d -p 8080:8080 --name spring-boot-admin darioseidl/spring-boot-admin-docker:2.0.1
+docker run -d -p 8080:8080 -e SPRING_SECURITY_USER_PASSWORD=YOUR_PASSWORD --name spring-boot-admin darioseidl/spring-boot-admin-docker:2.0.1
 `
 
 The admin UI is then available at <http://localhost:8080> (or <http://your-docker-ip:8080>).
 
+Login with `user` and your password.
+
 ### Build and run local image
 
-Build with:
+Build it with:
 
 ```
 ./gradlew bootJar
@@ -26,9 +28,9 @@ docker build -t spring-boot-admin-docker:2.0.1 docker
 
 ```
 
-Run with:
+Run it with (replace `YOUR_PASSWORD`):
 ```
-docker run -d -p 8080:8080 --name spring-boot-admin-docker spring-boot-admin-docker:2.0.1
+docker run -d -p 8080:8080 -e SPRING_SECURITY_USER_PASSWORD=YOUR_PASSWORD --name spring-boot-admin spring-boot-admin-docker:2.0.1
 ```
 
 ### Push
--- src/main/java/pl/aetas/springbootadmin/SpringBootAdminDockerApplication.java
+++ src/main/java/pl/aetas/springbootadmin/SpringBootAdminDockerApplication.java
@@ -1,15 +1,49 @@
 package pl.aetas.springbootadmin;
 
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
-@Configuration
 @EnableAutoConfiguration
 @EnableAdminServer
 public class SpringBootAdminDockerApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(SpringBootAdminDockerApplication.class, args);
+    }
+
+    // http://codecentric.github.io/spring-boot-admin/2.0.1/#_securing_spring_boot_admin_server
+    @Configuration
+    @EnableWebSecurity
+    public static class SecurityConfig extends WebSecurityConfigurerAdapter {
+        private final String adminContextPath;
+
+        public SecurityConfig(AdminServerProperties adminServerProperties) {
+            this.adminContextPath = adminServerProperties.getContextPath();
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            // @formatter:off
+            SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+            successHandler.setTargetUrlParameter("redirectTo");
+
+            http.authorizeRequests()
+                .antMatchers(adminContextPath + "/assets/**").permitAll()
+                .antMatchers(adminContextPath + "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
+            .logout().logoutUrl(adminContextPath + "/logout").and()
+            .httpBasic().and()
+            .csrf().disable();
+            // @formatter:on
+        }
     }
 }
